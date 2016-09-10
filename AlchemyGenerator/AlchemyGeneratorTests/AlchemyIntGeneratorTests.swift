4eaf147f8047c2af445467149168d47022411852
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-10 02:25:45 -0700

    Revert "Removes unnecessary test file"
    This reverts commit 420706e4fa20a59ab5db74fcd85a56d954ff23b3.

---
+++ AlchemyGenerator/AlchemyGeneratorTests/AlchemyIntGeneratorTests.swift
@@ -0,0 +1,97 @@
+//
+//  AlchemyIntGeneratorTests.swift
+//  AlchemyGenerator
+//
+//  Created by Wellington Moreno on 9/10/16.
+//  Copyright © 2016 Sir Wellington. All rights reserved.
+//
+
+import Foundation
+@testable import AlchemyGenerator
+import XCTest
+
+class AlchemyIntGeneratorTests: XCTestCase
+{
+    
+    override func setUp()
+    {
+        
+    }
+    
+    func testRandomNumberWithPositive()
+    {
+        var from = 0
+        var to = 100
+        
+        var result = AlchemyGenerator.integer(from: from, to: to)
+        verify(withMin: from, max: to, result: result)
+        
+        from = 10
+        to = 10_000
+        result = AlchemyGenerator.integer(from: from, to: to)
+        verify(withMin: from, max: to, result: result)
+        
+        from = 100
+        to = 100_000_000
+        result = AlchemyGenerator.integer(from: from, to: to)
+        verify(withMin: from, max: to, result: result)
+    }
+    
+    func testRandomNumberWithReversedParameters()
+    {
+        let from = 0
+        let to = 1000
+        
+        let result = AlchemyGenerator.integer(from: to, to: from)
+        verify(withMin: from, max: to, result: result)
+    }
+    
+    func testRandomNumberWithNegative()
+    {
+        var from = -100
+        var to = -1
+        var result = AlchemyGenerator.integer(from: from, to: to)
+        verify(withMin: from, max: to, result: result)
+        
+        from = -100_000
+        to = 0
+        result = AlchemyGenerator.integer(from: from, to: to)
+        verify(withMin: from, max: to, result: result)
+        
+        from = -100
+        to = 100
+        result = AlchemyGenerator.integer(from: from, to: to)
+    }
+    
+    func testRandomNumberWithNegativeAndReversedParameters()
+    {
+        let from = -100
+        let to = 0
+        let result = AlchemyGenerator.integer(from: to, to: from)
+        verify(withMin: from, max: to, result: result)
+    }
+    
+    private func verify(withMin min: Int, max: Int, result: Int)
+    {
+        XCTAssertTrue(result >= min)
+        XCTAssertTrue(result <= max)
+    }
+    
+    func testPositiveIntegers()
+    {
+        let result = AlchemyGenerator.positiveInteger()
+        XCTAssertTrue(result > 0)
+    }
+    
+    func testNegativeIntegers()
+    {
+        let result = AlchemyGenerator.negativeInteger()
+        XCTAssertTrue(result < 0)
+    }
+    
+    func testAnyInteger()
+    {
+        let result = AlchemyGenerator.anyInteger()
+        XCTAssertNotNil(result)
+    }
+}
