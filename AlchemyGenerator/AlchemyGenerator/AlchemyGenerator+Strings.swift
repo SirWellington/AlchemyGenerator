ccb6c80f0383e9999f99ddb535467cf1327e37e4
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-10 02:13:59 -0700

    Adds String Generators

---
+++ AlchemyGenerator/AlchemyGenerator/AlchemyGenerator+Strings.swift
@@ -0,0 +1,27 @@
+//
+//  AlchemyGenerator+Strings.swift
+//  AlchemyGenerator
+//
+//  Created by Wellington Moreno on 9/10/16.
+//  Copyright © 2016 Sir Wellington. All rights reserved.
+//
+
+import Foundation
+
+public extension AlchemyGenerator
+{
+    
+    fileprivate static var defaultSize: Int {
+        return AlchemyGenerator.integer(from: 5, to: 20)
+    }
+    
+    public class Strings
+    {
+        
+    }
+    
+    static func numericString(ofSize size: Int = defaultSize)
+    {
+        
+    }
+}
