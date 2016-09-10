ae162c7fc96533a5f6eab7c08daaef753b5ec8c5
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-10 01:12:10 -0700

    Adds initial project setup

---
+++ AlchemyGenerator/AlchemyGenerator/AlchemyGenerator.h
@@ -0,0 +1,19 @@
+//
+//  AlchemyGenerator.h
+//  AlchemyGenerator
+//
+//  Created by Wellington Moreno on 9/10/16.
+//  Copyright © 2016 Sir Wellington. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AlchemyGenerator.
+FOUNDATION_EXPORT double AlchemyGeneratorVersionNumber;
+
+//! Project version string for AlchemyGenerator.
+FOUNDATION_EXPORT const unsigned char AlchemyGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlchemyGenerator/PublicHeader.h>
+
+
