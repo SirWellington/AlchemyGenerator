710825c36f9870a4ed5dc6d6348013d6677efa6b
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-10 02:31:03 -0700

    Adds String Unit Test

--- AlchemyGenerator/AlchemyGeneratorTests/AlchemyGeneratorStringTests.swift
+++ AlchemyGenerator/AlchemyGeneratorTests/AlchemyGeneratorStringTests.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2016 Sir Wellington. All rights reserved.
 //
 
+import AlchemyGenerator
 import Foundation
+import XCTest
+
+class AlchemyStringGeneratorTests: XCTestCase
+{
+    private var size: Int = 0
+    
+    override func setUp()
+    {
+        size = AlchemyGenerator.integer(from: 10, to: 100)
+    }
+    
+    func testNumericString()
+    {
+        let string = AlchemyGenerator.numericString(ofSize: size)
+        XCTAssertEqual(string.characters.count, size)
+    }
+}
