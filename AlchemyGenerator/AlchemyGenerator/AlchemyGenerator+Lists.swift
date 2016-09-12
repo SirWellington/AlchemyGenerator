a18004bacdc2dfb9b8f7ab23f667ab6448ef53e3
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-11 17:07:58 -0700

    Adds Array Generators

---
+++ AlchemyGenerator/AlchemyGenerator/AlchemyGenerator+Lists.swift
@@ -0,0 +1,74 @@
+//
+//  AlchemyGenerator+Arrays.swift
+//  AlchemyGenerator
+//
+//  Created by Wellington Moreno on 9/11/16.
+//  Copyright © 2016 Sir Wellington. All rights reserved.
+//
+
+import Foundation
+
+//MARK: List Generation Classes
+public extension AlchemyGenerator
+{
+    public class Arrays
+    {
+        public static var ofString: [String]
+        {
+            return AlchemyGenerator.array()
+            {
+                return AlchemyGenerator.alphanumericString()
+            }
+        }
+        
+        public static var ofIntegers: [Int]
+        {
+            return AlchemyGenerator.array(withCreator: AlchemyGenerator.anyInteger)
+        }
+        
+        public static var ofPositiveIntegers: [Int]
+        {
+            return AlchemyGenerator.array(withCreator: AlchemyGenerator.positiveInteger)
+        }
+    }
+}
+
+
+//MARK: List Generation Methods
+public extension AlchemyGenerator
+{
+    fileprivate static var defaultSize: Int
+    {
+        return integer(from: 5, to: 50)
+    }
+    
+    static func array<T>(ofSize size: Int = defaultSize, withCreator creator: () -> T) -> [T]
+    {
+        guard size > 0 else { return [] }
+        
+        var result: [T] = []
+        
+        for _ in (1...size)
+        {
+            let newElement = creator()
+            result.append(newElement)
+        }
+        
+        return result
+    }
+    
+    static func set<T>(withAttemptedSize size: Int = defaultSize, withCreator creator: () -> T) -> Set<T>
+    {
+        guard size > 0 else { return Set() }
+        
+        var result: Set<T> = Set()
+        
+        for _ in 1...size
+        {
+            let newElement = creator()
+            result.insert(newElement)
+        }
+        
+        return result
+    }
+}
---
+++ AlchemyGenerator/AlchemyGeneratorTests/AlchemyListGeneratorTests.swift
@@ -0,0 +1,61 @@
+//
+//  AlchemyListGeneratorTests.swift
+//  AlchemyGenerator
+//
+//  Created by Wellington Moreno on 9/11/16.
+//  Copyright © 2016 Sir Wellington. All rights reserved.
+//
+
+import AlchemyGenerator
+import Foundation
+import XCTest
+
+class AlchemyListGeneratorTests: XCTestCase
+{
+    var iterations = 0
+    var size = 0
+    
+    override func setUp()
+    {
+        iterations = AlchemyGenerator.integer(from: 10, to: 100)
+        size = AlchemyGenerator.integer(from: 25, to: 200)
+    }
+    
+    func testArray()
+    {
+        self.repeat()
+        {
+            let result = AlchemyGenerator.array(ofSize: size)
+            {
+                return AlchemyGenerator.hexString()
+            }
+            
+            ensureNotEmpty(array: result)
+            XCTAssertEqual(result.count, size)
+        }
+    }
+    
+    fileprivate func `repeat`(_ operation: () -> ())
+    {
+        for _ in 1...iterations
+        {
+            operation()
+        }
+    }
+}
+
+//MARK: Test Lists Class
+extension AlchemyListGeneratorTests
+{
+    func testArraysClass()
+    {
+        ensureNotEmpty(array: AlchemyGenerator.Arrays.ofString)
+        ensureNotEmpty(array: AlchemyGenerator.Arrays.ofIntegers)
+        ensureNotEmpty(array: AlchemyGenerator.Arrays.ofPositiveIntegers)
+    }
+    
+    fileprivate func ensureNotEmpty<T>(array: [T])
+    {
+        XCTAssertFalse(array.isEmpty)
+    }
+}
