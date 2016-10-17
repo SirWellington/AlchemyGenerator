005589331086f3f6e48be43ce7656d64f374d903
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-10-16 22:05:49 -0700

    Adding OSX Build

---
+++ AlchemyGenerator/AlchemyGeneratorOSX/AlchemyGeneratorOSX.h
@@ -0,0 +1,19 @@
+//
+//  AlchemyGeneratorOSX.h
+//  AlchemyGeneratorOSX
+//
+//  Created by Wellington Moreno on 10/16/16.
+//  Copyright © 2016 SirWellington. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AlchemyGeneratorOSX.
+FOUNDATION_EXPORT double AlchemyGeneratorOSXVersionNumber;
+
+//! Project version string for AlchemyGeneratorOSX.
+FOUNDATION_EXPORT const unsigned char AlchemyGeneratorOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlchemyGeneratorOSX/PublicHeader.h>
+
+
