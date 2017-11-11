2486600473d2a3f78d3c796b563d17b84beb2c13
Wellington Moreno <jwellington.moreno@gmail.com>
2017-11-10 21:30:02 -0800

    Adds code to generate Doubles
    + Unit tests

---
+++ AlchemyGenerator/AlchemyGeneratorTests/AlchemyGenerator+DoubleTests.swift
@@ -0,0 +1,57 @@
+//
+//  AlchemyGenerator+DoubleTests.swift
+//  AlchemyGeneratorTests
+//
+//  Created by Wellington Moreno on 11/10/17.
+//  Copyright © 2017 SirWellington. All rights reserved.
+//
+
+import AlchemyGenerator
+import Foundation
+import XCTest
+
+class AlchemyGeneratorPlusDoubles_Tests: XCTestCase
+{
+    
+    func testRandomWithPositive()
+    {
+        testRandomWith(min: 0, max: 1000)
+        testRandomWith(min: 139, max: 129342)
+        testRandomWith(min: 100, max: 10_000_000.0)
+    }
+    
+    func testRandomWithNegative()
+    {
+        testRandomWith(min: -10, max: 0)
+        testRandomWith(min: -100, max: -10)
+        testRandomWith(min: -100_000, max: -1)
+    }
+    
+    func testRandomWithNegativeMinAndPositiveMax()
+    {
+        testRandomWith(min: -100, max: 100)
+        testRandomWith(min: -1, max: 1)
+    }
+    
+    func testRandomWithInversedParameters()
+    {
+        let min = 10.0
+        let max = 100.0
+        let result = AlchemyGenerator.doubles(fromInclusive: max, toInclusive: min)
+        verify(withMin: min, max: max, result: result)
+    }
+    
+    
+    
+    private func testRandomWith(min: Double, max: Double)
+    {
+        let result = AlchemyGenerator.doubles(fromInclusive: min, toInclusive: max)
+        verify(withMin: min, max: max, result: result)
+    }
+    
+    private func verify(withMin min: Double, max: Double, result: Double)
+    {
+        XCTAssertTrue(result >= min)
+        XCTAssertTrue(result <= max)
+    }
+}
