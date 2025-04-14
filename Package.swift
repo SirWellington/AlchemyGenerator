f7209a223ce5d006eaedcaed755439513db15426
Wellington Moreno <wellington@blacksource.tech>
2025-04-14 10:02:37 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,22 @@
 // swift-tools-version:5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "AlchemyGenerator",
     platforms: [
-        .macOS(.v12), .iOS(.v15)
+        .macOS(.v12),
+        .iOS(.v15)
     ],
     products: [
         .library(
             name: "AlchemyGenerator",
-            targets: ["AlchemyGenerator"]),
+            targets: ["AlchemyGenerator"]
+        )
     ],
     dependencies: [
         .package(
             url: "https://github.com/SirWellington/alchemytest",
-            branch: "develop"
+            .branch("develop")
         )
     ],
     targets: [
@@ -33,6 +33,6 @@ let package = Package(
                     package: "alchemytest"
                 )
             ]
-        ),
+        )
     ]
 )
